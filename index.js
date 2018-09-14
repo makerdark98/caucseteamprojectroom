0ac237e2a82f274a7da7619c144c85e829e5831a
Min-Uk.Lee <makerdark98@naver.com>
2018-09-14 23:53:59 +0900

    init

---
+++ index.js
@@ -0,0 +1,126 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const db = mongoose.connection;
+const mongourl = process.env.MONGOLAB_URI;
+mongoose.connect(mongourl);
+
+const eventSchema = mongoose.Schema({
+    title:String,
+    start: Date,
+    end:Date,
+    owner:String,
+    password:String,
+    phone:String
+});
+const logSchema = mongoose.Schema({
+    date: {type:Date, default:Date.now},
+    body: String
+});
+function getEventsBySameTime(start, end) {
+    return new Promise((resolve, reject) =>{
+        let count = 0;
+        Events.find({start: {$lte: start}, end: {$gt : start}}, (err, docs) =>{
+            if(err) reject(err);
+            count = count > docs.length ? count : docs.length;
+            Events.find({start: {$lt: end}, end: {$gte : end}}, (err, docs) =>{
+                if(err) reject(err);
+                count = count > docs.length ? count : docs.length;
+                Events.find({start: {$lt: end}, end: {$gt : start}}, (err, docs) =>{
+                    if(err) reject(err);
+                    count = count > docs.length ? count : docs.length;
+                    resolve(count);
+                });
+            });
+        });
+    });
+}
+function getEventsBySameTimeInit(start, end) {
+    let count = 0;
+    return new Promise((resolve, reject) =>{
+        Events.find({start: {$lte: start}, end: {$gt : start}}, (err, docs) =>{
+            if(err) reject(err);
+            count = count > docs.length ? count : docs.length;
+            Events.find({start: {$lt: end}, end: {$gte : end}}, (err, docs) =>{
+                if(err) reject(err);
+                count = count > docs.length ? count : docs.length;
+                Events.find({start: {$lt: end}, end: {$gt : start}}, (err, docs) =>{
+                    if(err) reject(err);
+                    count = count > docs.length ? count : docs.length;
+                    Events.find({start: {$lte: start}, end: {$gte : end}}, (err, docs) =>{
+                        if(err) reject(err);
+                        if(docs.length>1){
+                            let cntarray = [];
+                            docs.forEach(doc => {
+                                let tmp = getEventsBySameTime(doc.start, doc.end);
+                                tmp.then(cnt => {
+                                    count = count > docs.length ? count : docs.length;
+                                });
+                                cntarray.push(tmp);
+                            });
+                            Promise.all(cntarray).then(()=>{resolve(count);});
+                        }
+                        else resolve(count);
+                    });
+                });
+            });
+        });
+    });
+}
+
+let Events = mongoose.model("event", eventSchema);
+let Logs = mongoose.model("log", logSchema);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/events", (req, res, next) => {
+    Events.find({}, (err, collection) => {
+        let pw = "password";
+        let phone = "phone";
+        let new_data = [];
+        collection.forEach(element => {
+            new_element = {
+                title: element.title,
+                start: element.start,
+                end: element.end
+            };
+            new_data.push(new_element);
+        });
+        res.send(new_data);
+    });
+});
+
+app.post("/events", (req, res, next) => {
+    new Logs({body:JSON.stringify(req.body)}).save();
+    let start = new Date(req.body.start);
+    let end = new Date(req.body.end);
+    let diff = (end.getTime() - start.getTime()) / (1000*60*60);
+    if(diff>3) {
+        res.send("<h1>룰을 확인하시고 다시 시도해주세요</h1><h2>시간규칙 위반</h2><a href='/'>뒤로가기</a>");
+        return;
+    }
+    let daydiff = (start.getTime() - Date.now()) / (1000*60*60*24);
+    if(daydiff>10){
+        res.send("<h1>룰을 확인하시고 다시 시도해주세요</h1><h2>날짜규칙 위반</h2><a href='/'>뒤로가기</a>");
+        return;
+    }
+    getEventsBySameTimeInit(start, end).then( cnt => {
+        //console.log(cnt);
+        if(cnt>=2) res.send("<h1>룰을 확인하시고 다시 시도해주세요</h1><h2>최대 동시 2팀</h2><a href='/'>뒤로가기</a>");
+        else (new Events(req.body)).save((err,event) => res.redirect("/index.html"));
+    });
+});
+app.post("/delete", (req, res) => {
+    new Logs({body:JSON.stringify(req.body)}).save();
+    Events.deleteOne({"start":req.body.start,"end":req.body.end,"password":req.body.password}, () => {
+        res.redirect("/index.html");
+    });
+});
+
+app.listen(process.env.PORT || 3000);
+
---
+++ public/index.js
@@ -0,0 +1,95 @@
+function postUrl(url, params){
+    var method = "post";
+    var form = document.createElement("form");
+    form.setAttribute("method", method);
+    form.setAttribute("action", url);
+    for(var key in params){
+        var hiddenField = document.createElement("input");
+        hiddenField.setAttribute("type","hidden");
+        hiddenField.setAttribute("name", key);
+        hiddenField.setAttribute("value", params[key]);
+
+        form.appendChild(hiddenField);
+    }
+    document.body.appendChild(form);
+    form.submit();
+}
+function numEvents(date)
+{
+    totalEvents = $('#calendar').fullCalendar('clientEvents').length;
+    var count = 0;
+    for (i=0;i<totalEvents;i++){
+        var event = $("#calendar").fullCalendar("clientEvents")[i];
+        var starttime =new Date(event.start).getTime();
+        var endtime = new Date(event.end).getTime();
+        if (starttime<= date.getTime() && endtime > date.getTime()) {
+            count++;
+        }
+    }
+    return count;
+}
+
+$(document).ready(function() {
+    $.get("/events").done(function(data) {
+        $("#calendar").fullCalendar({
+            defaultView: "agendaWeek",
+            minTime: "09:00:00",
+            maxTime: "21:00:00",
+            nowIndicator: true,
+            now: new Date(),
+            selectable: true,
+            header: { right: "today prev next, agendaWeek", left: ""},
+            events: data,
+            eventRender: function(event, element){
+                $(element).addTouch();
+            },
+            eventAfterRender: function(event, element, view) {
+                var width = $(element).width();
+                var numevent = numEvents(new Date(event.start));
+                if(numevent==1) width = (width/2);
+                $(element).css('width', width+'px');
+            },
+            select: function(start, end) {
+                if((new Date(end).getTime()-new Date(start).getTime()) / (1000*60*60) > 3){
+                    alert("3시간 초과는 신청 불가능 합니다.");
+                    return;
+                }
+                var owner = prompt("사용자 이름을 넣어주세요");
+                if(owner === null ) return;
+                var phone = prompt("핸드폰 번호를 넣어주세요(비상연락수단)");
+                if(phone === null ) return;
+                var title = prompt("책임자의 이름을 적어주세요(캘린더 표기)");
+                if(title === null ) return;
+                var password = prompt("본인확인 비밀번호를 적어주세요(평소에 쓰는 비밀번호 기재 금지)");
+                if(password === null) return;
+                if(owner === null || title === null || password == null) return;
+                var params = {
+                    owner: owner,
+                    phone: phone,
+                    title: title,
+                    password: password,
+                    start: start,
+                    end: end
+                }
+                postUrl("/events", params);
+            },
+            eventClick: function(calEvent, jsEvent, view) {
+                var password = prompt("본인확인 비밀번호를 적어주세요(평소에 쓰는 비밀번호 기재 금지)");
+                var params = {
+                    password: password,
+                    start: calEvent.start,
+                    end: calEvent.end
+                }
+                postUrl("/delete", params);
+            }
+        });
+    });
+    $("#dialog").dialog({
+        modal: true,
+        autoOpen:false,
+        title: "예약 신청",
+    });
+    $("#reservation").click(function () {
+        $("#dialog").dialog("open");
+    });
+});
